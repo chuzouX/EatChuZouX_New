bf3f8818a5fae687f805b587f4bf32dc48787149
chuzouX <75435667+chuzouX@users.noreply.github.com>
2022-09-18 09:57:07 +0800

    Update index.js

--- Easter_eggs/skin1/static/index.js
+++ Easter_eggs/skin1/static/index.js
@@ -14,7 +14,7 @@ let __Time = 20;
 let __k = 4;
 let _close = false; 
 let _fsj = false;
-var url = 'https://chuzoux.github.io/EatChuZouX_New/Easter_eggs/sink1/static/image/ClickBefore.png';
+var url = 'https://chuzoux.github.io/EatChuZouX_New/Easter_eggs/skin1/static/image/ClickBefore.png';
 
 function isplaying() {
     return document.getElementById('welcome').style.display == 'none' &&
